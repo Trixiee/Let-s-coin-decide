361152afb6d45a6c255585cb9499382f0997fa23
Mirjana Šuput <mirjanasuput4@gmail.com>
2017-07-02 16:23:14 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let rotation
+let timer
+
+
+
+function start ()  {
+	let i=0;
+	let j =0;
+
+	
+   
+	let change = Math.random() < 0.5
+
+	clearInterval(rotation)
+	clearInterval(timer)
+
+	rotation = setInterval(
+		function(){ 
+			circle.style = `transform: rotateY(${j++}deg)`},1
+		);
+
+	
+	timer = setInterval(function(){
+		if(change){
+			circle.setAttribute("src", "images/coinhead-image.jpg")
+			messages= 'head'
+		}else{
+			circle.setAttribute("src", "images/cointail-image.png")
+
+
+		}
+		
+		change = !change
+
+		if(++i>80*Math.random()) {
+
+			clearInterval(timer)
+			clearInterval(rotation)
+			circle.style = `transform: rotateY(0deg)`
+		
+
+		}
+	},360)  
+		 
+
+
+}
